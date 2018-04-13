aae17ee303292b0b750b9eb85549a952873784e5
Amir Hammad <hi@amir.sk>
2018-04-13 21:12:04 +0200

    check manifest manually
    Signed-off-by: Amir Hammad <hi@amir.sk>

--- scripts/contains.sh
+++ scripts/contains.sh
@@ -2,3 +2,6 @@
 set -x 
 set -e
 grep -C 5 "READ_PHONE_STATE" platforms/android/build/outputs/logs/manifest-merger-debug-report.txt
+
+
+printf "\n\n\nVerify presence in manifest: platforms/android/build/intermediates/manifests/full/debug/AndroidManifest.xml\n" 
